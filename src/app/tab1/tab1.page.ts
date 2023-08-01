e0857d5ceb71ea6d79997052e2f2994e0d9f2ed3
Eduarto Shurdha <eduarto.shurdha@atis.al>
2023-08-01 12:53:54 +0200

    init

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  providers: [InAppBrowser],
 })
 export class Tab1Page {
+  constructor(private iab: InAppBrowser) {}
 
-  constructor() {}
-
+  asdf() {
+    const browser = this.iab.create(
+      'http://adrspares.ricambio.net/site/pagece5.wplus?ID_COUNT=ce_5_matricola&LN=1&CEPV=ADR001&CELN=1'
+    );
+    browser.on('loadstop').subscribe(() => {
+      browser.executeScript({ code: 'alert("asdf");' });
+    });
+  }
 }
